cdc8b85ad43d631bb06ffa7ca5a0270d0e87b352
kasrakhosravi <kasrakhosravi2@gmail.com>
2024-04-30 10:38:53 +0200

    wip: input sql

--- src/aave/enrichers/interval-enricher/handler.ts
+++ src/aave/enrichers/interval-enricher/handler.ts
@@ -1,18 +1,23 @@
 import { calculateAaveValues } from "../../utils.js";
 import { Customizations } from '../../../lib/customizations.js'
 
-export async function handleInput() {
-  const markets = [
-    { chainId: 1, address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48' }
-  ];
+export async function handleInput({ data }) {
+  if (!data) {
+    throw new Error(
+      `Skipping processing item for aave-notifications, missing assetAddress : ${JSON.stringify(
+        { data }
+      )}`
+    );
+  }
 
-  for (const market of markets) {
-    const values = await calculateAaveValues(market?.address, market?.chainId)
+  const aaveValues = await calculateAaveValues(
+    data.assetAddress,
+    data.chainId
+  );
 
-    if (Customizations?.onBorrowAPY) {
-      await Customizations.onBorrowAPY(market?.address, values?.variableBorrowAPY)
-    }
-  };
+  if (Customizations?.onBorrowAPY) {
+    await Customizations.onBorrowAPY(data?.address, aaveValues?.variableBorrowAPY)
+  }
 
   return true;
 }
