a451e3e5253bd93cb96b0073f7e4b8cb63555ad2
kasrakhosravi <kasrakhosravi2@gmail.com>
2024-05-02 19:21:40 +0200

    wip: borrow-apy-change-tracker

--- src/processors/borrow-apy-change-tracker/handler.ts
+++ src/processors/borrow-apy-change-tracker/handler.ts
@@ -3,21 +3,14 @@ import { Customizations } from '../../lib/customizations.js';
 import { calculateAPYValues } from "../../functions/aave.js";
 
 export const processEvent = async (event: EventHandlerInput) => {
-
-  const isAaveEvent =
-    event.parsed?.args?.variableBorrowRate &&
-    event.parsed?.args?.stableBorrowRate;
-
-  if (isAaveEvent) {
-    const { variableBorrowAPY } = await calculateAPYValues(
-      event.parsed?.args?.liquidityRate, 
-      event.parsed?.args?.variableBorrowRate,
-      event.parsed?.args?.stableBorrowRate,
-    );
-    console.debug(`Aave Borrow APY: ${variableBorrowAPY}-${event.parsed?.args?.reserve}-${event.parsed?.args?.user}`);
-    if (Customizations?.onBorrowAPY) {
-      await Customizations?.onBorrowAPY(event.parsed?.args?.reserve, 'aave', variableBorrowAPY);
-    }
+  const { variableBorrowAPY } = await calculateAPYValues(
+    event.parsed?.args?.liquidityRate, 
+    event.parsed?.args?.variableBorrowRate,
+    event.parsed?.args?.stableBorrowRate,
+  );
+  console.debug(`Aave Borrow APY: ${variableBorrowAPY}-${event.parsed?.args?.reserve}-${event.parsed?.args?.user}`);
+  if (Customizations?.onBorrowAPY) {
+    await Customizations?.onBorrowAPY(event.parsed?.args?.reserve, 'aave', variableBorrowAPY);
   }
 
   return true;
