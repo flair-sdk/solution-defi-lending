97096b4e210ebcf2a9cab27da5b0d4ba2288ff3a
kasrakhosravi <kasrakhosravi2@gmail.com>
2024-05-03 15:32:34 +0200

    wip: borrow-apy-change-tracker

--- index.ts
+++ index.ts
@@ -51,16 +51,72 @@ const definition: SolutionDefinition<Config> = {
         filterGroup.addresses = [
           ...(filterGroup.addresses || []),
           {
-            fromFile: `${PACKAGE_NAME}/src/processors/borrow-apy-change-tracker/contracts.csv`,
+            chainId: "1",
+            address: "0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2",
+          },
+          {
+            chainId: "10",
+            address: "0x794a61358D6845594F94dc1DB02A252b5b4814aD",
+          },
+          {
+            chainId: "42161",
+            address: "0x794a61358D6845594F94dc1DB02A252b5b4814aD",
+          },
+          {
+            chainId: "137",
+            address: "0x794a61358D6845594F94dc1DB02A252b5b4814aD",
+          },
+          {
+            chainId: "250",
+            address: "0x794a61358D6845594F94dc1DB02A252b5b4814aD",
+          },
+          {
+            chainId: "43114",
+            address: "0x794a61358D6845594F94dc1DB02A252b5b4814aD",
+          },
+          {
+            chainId: "1666600000",
+            address: "0x794a61358D6845594F94dc1DB02A252b5b4814aD",
+          },
+          {
+            chainId: "1088",
+            address: "0x90df02551bB792286e8D4f13E0e357b4Bf1D6a57",
+          },
+          {
+            chainId: "8453",
+            address: "0xA238Dd80C259a72e81d7e4664a9801593F98d1c5",
+          },
+          {
+            chainId: "100",
+            address: "0xb50201558B00496A145fE76f7424749556E326D8",
+          },
+          {
+            chainId: "56",
+            address: "0x6807dc923806fE8Fd134338EABCA509979a7e0cB",
+          },
+          {
+            chainId: "534352",
+            address: "0x11fCfe756c05AD438e312a7fd934381537D3cFfe",
           },
         ];
 
         manifest.filterGroups = [
+          ...manifest.filterGroups.filter(
+            (group) => group.id !== config.addAddressesToFilterGroup
+          ),
           filterGroup,
         ];
       }
 
-      console.debug('manifest.filterGroups', manifest.filterGroups[0].addresses);
+      console.debug(
+        "manifest.filterGroups",
+        manifest.filterGroups[0].addresses
+      );
+
+      console.debug(
+        "manifest.filterGroups",
+        manifest.filterGroups[1].addresses
+      );
 
       manifest.processors = [
         ...(manifest.processors || []),
